33cc51e8ff139ee8c6175ec68b2db8a358166d53
Radmir Khurum <radmirkhurum@gmail.com>
2024-04-14 15:48:49 +0700

    client payloads done

---
+++ clinic-service/src/main/java/ru/rrk/clinic/payload/Client/UpdateClientPayload.java
@@ -0,0 +1,24 @@
+package ru.rrk.clinic.payload.Client;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateClientPayload(
+        @NotNull(message = "{clinic.clients.update.errors.first_name_is_null}")
+        @Size(max = 50)
+        String firstName,
+
+        @Nullable
+        @Size(max = 50, message = "{clinic.clients.update.errors.last_name_size_is_invalid}")
+        String lastName,
+
+        @NotNull(message = "{clinic.clients.update.errors.phone_number_is_null}")
+        @Size(max = 20)
+        String phoneNumber,
+
+        @Nullable
+        @Size(max = 255)
+        String email
+) {
+}
