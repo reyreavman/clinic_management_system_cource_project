818a05e0d92b7792c738986f40a0266c5531a8ce
Radmir Khurum <radmirkhurum@gmail.com>
2024-04-14 16:05:27 +0700

    ClientsRestController done
    Clients all done

---
+++ clinic-service/src/main/java/ru/rrk/clinic/controller/ClientsRestController.java
@@ -0,0 +1,43 @@
+package ru.rrk.clinic.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.rrk.clinic.controller.payload.Client.NewClientPayload;
+import ru.rrk.clinic.entity.Client;
+import ru.rrk.clinic.services.client.ClientService;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("clinic-api/clients")
+public class ClientsRestController {
+    private final ClientService service;
+
+    @GetMapping
+    public Iterable<Client> findProducts(@RequestParam(name = "filter", required = false) String filter) {
+        return this.service.findAllClients(filter);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createClient(@Valid @RequestBody NewClientPayload payload,
+                                          BindingResult bindingResult,
+                                          UriComponentsBuilder uriComponentsBuilder) throws BindException {
+        if (bindingResult.hasErrors()) {
+            if (bindingResult instanceof BindException exception) throw exception;
+            else throw new BindException(bindingResult);
+        } else {
+            Client client = this.service.createClient(payload.firstName(), payload.lastName(), payload.phoneNumber(), payload.email());
+            return ResponseEntity
+                    .created(uriComponentsBuilder
+                            .replacePath("/clinic-api/clients/{clientId}")
+                            .build(Map.of("clientId", client.getId())))
+                    .body(client);
+        }
+    }
+}
