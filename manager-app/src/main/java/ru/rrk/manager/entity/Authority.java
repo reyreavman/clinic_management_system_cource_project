8502cc74a33d9b282bc58f7c8e3154c21b2a76d1
Radmir Khurum <radmirkhurum@gmail.com>
2024-04-15 05:35:02 +0700

    entities list in progress

--- manager-app/src/main/java/ru/rrk/manager/entity/Authority.java
+++ manager-app/src/main/java/ru/rrk/manager/entity/Authority.java
@@ -1,4 +1,20 @@
 package ru.rrk.manager.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "user_management", name = "t_user")
 public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "c_authority")
+    private String authority;
 }
