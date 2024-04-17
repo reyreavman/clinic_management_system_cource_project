af1e18623156e427df4545a69953ffa88315d55f
Radmir Khurum <radmirkhurum@gmail.com>
2024-04-17 21:59:34 +0700

    templates.client.done

--- manager-app/src/main/java/ru/rrk/manager/entity/Client.java
+++ manager-app/src/main/java/ru/rrk/manager/entity/Client.java
@@ -1,4 +1,7 @@
 package ru.rrk.manager.entity;
 
-public record Client(int id, String firstName, String lastName, String phoneNumber, String email) {
+public record Client(int id, String firstName, String lastName, String phoneNumber, String email, String fullName) {
+    Client(int id, String firstName, String lastName, String phoneNumber, String email) {
+        this(id, firstName, lastName, phoneNumber, email, firstName + " " + lastName);
+    }
 }
