9e5c1f0f85583e82fbfe691601cea83f77be712a
reyreavman <radmirkhurum@gmail.com>
2024-05-06 16:16:55 +0700

    gender added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/entity/Gender.java
@@ -0,0 +1,21 @@
+package ru.rrk.clinic.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(schema = "clinic", name = "t_gender")
+public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "c_gender")
+    @NotNull
+    private String gender;
+}
