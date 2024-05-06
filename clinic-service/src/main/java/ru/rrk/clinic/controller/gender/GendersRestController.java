28b44b7f3aa50f45c211c8c56494129451bfb5d3
reyreavman <radmirkhurum@gmail.com>
2024-05-06 16:48:00 +0700

    GendersRestController.java created

---
+++ clinic-service/src/main/java/ru/rrk/clinic/controller/gender/GendersRestController.java
@@ -0,0 +1,46 @@
+package ru.rrk.clinic.controller.gender;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.rrk.clinic.entity.Gender;
+import ru.rrk.clinic.service.gender.GenderService;
+import ru.rrk.manager.controller.genders.payload.NewGenderPayload;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("clinic-api/genders")
+public class GendersRestController {
+    private final GenderService service;
+
+    @GetMapping
+    public Iterable<Gender> findGenders(Principal principal) {
+        LoggerFactory.getLogger(GendersRestController.class).info("Principal: {}", principal);
+        return this.service.findAllGenders();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createGender(@Valid @RequestBody NewGenderPayload payload,
+                                          BindingResult bindingResult,
+                                          UriComponentsBuilder uriComponentsBuilder) throws BindException {
+        if (bindingResult.hasErrors()) {
+            if (bindingResult instanceof BindException exception) throw exception;
+            else throw new BindException(bindingResult);
+        } else {
+            Gender gender = this.service.createGender(payload.gender());
+            return ResponseEntity
+                    .created(uriComponentsBuilder
+                            .replacePath("/clinic-api/genders/{genderId}")
+                            .build(Map.of("genderId", gender.getId())))
+                    .body(gender);
+        }
+    }
+}
