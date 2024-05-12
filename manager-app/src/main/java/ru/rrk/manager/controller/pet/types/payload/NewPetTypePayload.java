5a989dc7f4c9dcde3d53a8502b685b4ec71c2212
reyreavman <radmirkhurum@gmail.com>
2024-05-12 22:10:23 +0700

    payloads added

---
+++ manager-app/src/main/java/ru/rrk/manager/controller/pet/types/payload/NewPetTypePayload.java
@@ -0,0 +1,4 @@
+package ru.rrk.manager.controller.pet.types.payload;
+
+public record NewPetTypePayload(String name) {
+}
---
+++ manager-app/src/main/java/ru/rrk/manager/controller/pet/types/payload/UpdatePetTypePayload.java
@@ -0,0 +1,4 @@
+package ru.rrk.manager.controller.pet.types.payload;
+
+public record UpdatePetTypePayload(String name) {
+}
