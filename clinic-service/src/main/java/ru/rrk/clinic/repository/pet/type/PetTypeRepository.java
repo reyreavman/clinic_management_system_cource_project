b657444451848c5d377a18c6f4800eee9084edd1
reyreavman <radmirkhurum@gmail.com>
2024-05-12 20:59:05 +0700

    repo added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/repository/pet/type/PetTypeRepository.java
@@ -0,0 +1,7 @@
+package ru.rrk.clinic.repository.pet.type;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.rrk.clinic.entity.pet.PetType;
+
+public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
+}
