79b8ce3c9a08e21529132eaa91eeb06f9ac76010
reyreavman <radmirkhurum@gmail.com>
2024-05-12 21:01:46 +0700

    repo modified

--- clinic-service/src/main/java/ru/rrk/clinic/repository/pet/type/PetTypeRepository.java
+++ clinic-service/src/main/java/ru/rrk/clinic/repository/pet/type/PetTypeRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import ru.rrk.clinic.entity.pet.PetType;
 
 public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
+    Iterable<PetType> findAllByName(String filter);
 }
