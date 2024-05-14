f3501ce17508bbc02474ded4af1a2813e922c8f5
reyreavman <radmirkhurum@gmail.com>
2024-05-14 14:41:28 +0700

    entity added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/entity/appointment/AppointmentsResultState.java
@@ -0,0 +1,21 @@
+package ru.rrk.clinic.entity.appointment;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "clinic", name = "t_appointment_results_state")
+public class AppointmentsResultState {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "c_state_name")
+    private String name;
+}
