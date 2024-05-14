856897969418d5ddbd1bf46d4620b70d037b5d02
reyreavman <radmirkhurum@gmail.com>
2024-05-14 19:49:48 +0700

    entity added

---
+++ manager-app/src/main/java/ru/rrk/manager/entity/checkup/Checkup.java
@@ -0,0 +1,12 @@
+package ru.rrk.manager.entity.checkup;
+
+import lombok.Builder;
+import ru.rrk.manager.entity.pet.Pet;
+import ru.rrk.manager.entity.vet.Vet;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Builder
+public record Checkup(int id, LocalDate date, LocalTime time, Pet pet, Vet vet, CheckupType checkupType, CheckupState checkupState, CheckupResult checkupResult) {
+}
