2937ee99f1629978ca9f62d740cc7e2976e94297
reyreavman <radmirkhurum@gmail.com>
2024-05-14 18:42:10 +0700

    entity added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/entity/checkup/Checkup.java
@@ -0,0 +1,52 @@
+package ru.rrk.clinic.entity.checkup;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.sql.results.graph.collection.internal.EagerCollectionAssembler;
+import ru.rrk.clinic.entity.pet.Pet;
+import ru.rrk.clinic.entity.vet.Vet;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "clinic", name = "t_checkup")
+public class Checkup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "c_checkup_date")
+    @NotNull
+    private LocalDate date;
+
+    @Column(name = "c_checkup_time")
+    @NotNull
+    private LocalTime time;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "c_pet_id")
+    @NotNull
+    private Pet pet;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "c_vet_id")
+    @NotNull
+    private Vet vet;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "c_type_id")
+    @NotNull
+    private CheckupType checkupType;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "c_state_id")
+    @NotNull
+    private CheckupState checkupState;
+}
