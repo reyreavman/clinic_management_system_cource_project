7db7901c41e0bc7ec274c2c952079091b180bab2
reyreavman <radmirkhurum@gmail.com>
2024-05-15 18:01:48 +0700

    payloads added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/controller/appointment/appointment/payload/NewAppointmentPayload.java
@@ -0,0 +1,20 @@
+package ru.rrk.clinic.controller.appointment.appointment.payload;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record NewAppointmentPayload(
+        @NotNull
+        Integer petId,
+        @NotNull
+        Integer vetId,
+        @NotNull
+        LocalDate date,
+        @NotNull
+        LocalTime time,
+        String description,
+        Integer checkupId
+) {
+}
