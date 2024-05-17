a01f5a868323d0eb4dd3acf6316e22bb3e69b08b
reyreavman <radmirkhurum@gmail.com>
2024-05-18 04:11:58 +0700

    payloads added

---
+++ clinic-service/src/main/java/ru/rrk/clinic/controller/appointment/result/payload/UpdateAppointmentResultPayload.java
@@ -0,0 +1,15 @@
+package ru.rrk.clinic.controller.appointment.result.payload;
+
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateAppointmentResultPayload(
+        @NotNull
+        Integer currentAppointmentId,
+        Integer nextAppointmentId,
+        @NotNull
+        Integer stateId,
+        Integer diagnosisId,
+        String advice,
+        String prescription
+) {
+}
