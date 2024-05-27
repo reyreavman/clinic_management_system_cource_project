03859309807a7cb53ac595aba508a3886f5bab94
reyreavman <radmirkhurum@gmail.com>
2024-05-27 15:44:36 +0700

    rest clients added

---
+++ user-app/src/main/java/ru/rrk/user/receptionist/restClient/AppointmentRestClient.java
@@ -0,0 +1,38 @@
+package ru.rrk.user.receptionist.restClient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+import ru.rrk.user.receptionist.entity.Appointment;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class AppointmentRestClient {
+    private static final ParameterizedTypeReference<List<Appointment>> APPOINTMENT_TYPE_REFERENCE = new ParameterizedTypeReference<List<Appointment>>() {
+    };
+    private final RestClient restClient;
+
+    public List<Appointment> findAllAppointments() {
+        return this.restClient
+                .get()
+                .uri("clinic-api/appointments")
+                .retrieve()
+                .body(APPOINTMENT_TYPE_REFERENCE);
+    }
+
+    public Optional<Appointment> findAppointment(Integer appointmentId) {
+        try {
+            return Optional.ofNullable(
+                    this.restClient
+                            .get()
+                            .uri("clinic-api/appointments/{appointmentId}", appointmentId)
+                            .retrieve()
+                            .body(Appointment.class));
+        } catch (HttpClientErrorException.NotFound exception) {
+            return Optional.empty();
+        }
+    }
+}
