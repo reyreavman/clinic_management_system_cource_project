33e255a80c507dd1feedb9ef8ba7b733b0b698a5
reyreavman <radmirkhurum@gmail.com>
2024-05-27 15:48:07 +0700

    rest client configured

--- user-app/src/main/java/ru/rrk/user/receptionist/config/RestClientBeansConfig.java
+++ user-app/src/main/java/ru/rrk/user/receptionist/config/RestClientBeansConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedCli
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 import ru.rrk.user.receptionist.restClient.AppointmentRestClient;
+import ru.rrk.user.receptionist.restClient.CheckupRestClient;
 import ru.rrk.user.receptionist.restClient.ReceptionistRestClient;
 import ru.rrk.user.receptionist.restClient.VetRestClient;
 import ru.rrk.user.receptionist.security.OAuthClientHttpRequestInterceptor;
@@ -51,6 +52,21 @@ public class RestClientBeansConfig {
             OAuth2AuthorizedClientRepository authorizedClientRepository,
             @Value("${kupang.services.clinic.registration-id:keycloak}") String registrationId) {
         return new AppointmentRestClient(RestClient.builder()
+                .baseUrl(clinicBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(
+                                        clientRegistrationRepository, authorizedClientRepository), registrationId))
+                .build());
+    }
+
+    @Bean
+    public CheckupRestClient checkupRestClient(
+            @Value("${kupang.services.clinic.uri:http://127.0.0.1:8081}") String clinicBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${kupang.services.clinic.registration-id:keycloak}") String registrationId) {
+        return new CheckupRestClient(RestClient.builder()
                 .baseUrl(clinicBaseUri)
                 .requestInterceptor(
                         new OAuthClientHttpRequestInterceptor(
