5be8d75c825c16af05bc38db1b0cd2a77aed0273
reyreavman <radmirkhurum@gmail.com>
2024-05-27 15:57:15 +0700

    configured

--- user-app/src/main/java/ru/rrk/user/receptionist/config/ConvertersConfig.java
+++ user-app/src/main/java/ru/rrk/user/receptionist/config/ConvertersConfig.java
@@ -3,11 +3,17 @@ package ru.rrk.user.receptionist.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.rrk.user.receptionist.converter.AppointmentDTOConverter;
+import ru.rrk.user.receptionist.converter.CheckupDTOConverter;
 
 @Configuration
 public class ConvertersConfig {
     @Bean
     public AppointmentDTOConverter appointmentDTOConverter() {
         return new AppointmentDTOConverter();
+    }
+
+    @Bean
+    public CheckupDTOConverter checkupDTOConverter() {
+        return new CheckupDTOConverter();
     }
 }
