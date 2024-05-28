16c9985bfb11682f37bcee3cf3298d1f1328325b
reyreavman <radmirkhurum@gmail.com>
2024-05-28 11:36:51 +0700

    converters configured

--- user-app/src/main/java/ru/rrk/user/receptionist/config/ConvertersConfig.java
+++ user-app/src/main/java/ru/rrk/user/receptionist/config/ConvertersConfig.java
@@ -2,18 +2,41 @@ package ru.rrk.user.receptionist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.rrk.user.receptionist.converter.AppointmentDTOConverter;
-import ru.rrk.user.receptionist.converter.CheckupDTOConverter;
+import ru.rrk.user.receptionist.mapper.*;
+import ru.rrk.user.receptionist.mapper.appointment.AppointmentViewPrimaryConverter;
+import ru.rrk.user.receptionist.mapper.appointment.AppointmentViewSummaryConverter;
+import ru.rrk.user.receptionist.mapper.checkup.CheckupViewPrimaryConverter;
+import ru.rrk.user.receptionist.mapper.checkup.CheckupViewSummaryConverter;
 
 @Configuration
 public class ConvertersConfig {
     @Bean
-    public AppointmentDTOConverter appointmentDTOConverter() {
-        return new AppointmentDTOConverter();
+    public AppointmentViewSummaryConverter appointmentViewPrimaryConverter() {
+        return new AppointmentViewSummaryConverter();
     }
 
     @Bean
-    public CheckupDTOConverter checkupDTOConverter() {
-        return new CheckupDTOConverter();
+    public AppointmentViewPrimaryConverter appointmentViewSummaryConverter() {
+        return new AppointmentViewPrimaryConverter();
+    }
+
+    @Bean
+    public CheckupViewSummaryConverter checkupViewSummaryConverter() {
+        return new CheckupViewSummaryConverter();
+    }
+
+    @Bean
+    public CheckupViewPrimaryConverter checkupViewPrimaryConverter() {
+        return new CheckupViewPrimaryConverter();
+    }
+
+    @Bean
+    public PetViewSummaryConverter petViewSummaryConverter() {
+        return new PetViewSummaryConverter();
+    }
+
+    @Bean
+    public VetViewSummaryConverter vetViewSummaryConverter() {
+        return new VetViewSummaryConverter();
     }
 }
