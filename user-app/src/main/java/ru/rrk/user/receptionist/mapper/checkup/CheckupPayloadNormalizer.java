7c7db47797224325eaba1e1c9f1e94cebd78e90e
reyreavman <radmirkhurum@gmail.com>
2024-05-29 21:00:38 +0700

    checkup normalizer added

--- user-app/src/main/java/ru/rrk/user/receptionist/mapper/checkup/CheckupPayloadNormalizer.java
+++ user-app/src/main/java/ru/rrk/user/receptionist/mapper/checkup/CheckupPayloadNormalizer.java
@@ -1,22 +1,23 @@
 package ru.rrk.user.receptionist.mapper.checkup;
 
 import lombok.RequiredArgsConstructor;
-import ru.rrk.user.receptionist.controller.payload.NewCheckupPayload;
+import org.springframework.transaction.annotation.Transactional;
+import ru.rrk.user.receptionist.controller.payload.NewCheckupDetailsPayload;
+import ru.rrk.user.receptionist.controller.payload.NewCheckupSummaryPayload;
+import ru.rrk.user.receptionist.dto.checkup.CheckupState;
 import ru.rrk.user.receptionist.restClient.checkup.CheckupResultRestClient;
 import ru.rrk.user.receptionist.restClient.checkup.CheckupStateRestClient;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 public class CheckupPayloadNormalizer {
     private final CheckupStateRestClient stateRestClient;
     private final CheckupResultRestClient resultRestClient;
 
-    public NewCheckupPayload normalizePayload(NewCheckupPayload payload) {
-        if (payload.getStateId() == null) {
-            this.stateRestClient.findAllStates().stream().filter(state -> state.state().toLowerCase().equals("ожидается")).findFirst().ifPresent(state -> payload.setStateId(state.id()));
-        }
-        if (payload.getResultId() == null) {
-            payload.setResultId(this.resultRestClient.createResult(null).id());
-        }
-        return payload;
+    public NewCheckupDetailsPayload normalizePayload(NewCheckupSummaryPayload payload) {
+        Integer currentStateId = this.stateRestClient.findAllStates().stream().filter(state -> state.state().equalsIgnoreCase("ожидается")).findFirst().map(CheckupState::id).orElseThrow(NoSuchElementException::new);
+        Integer currentResultId = this.resultRestClient.createResult(null).id();
+        return new NewCheckupDetailsPayload(payload.date(), payload.time(), payload.petId(), payload.vetId(), payload.typeId(), currentStateId, currentResultId);
     }
 }
