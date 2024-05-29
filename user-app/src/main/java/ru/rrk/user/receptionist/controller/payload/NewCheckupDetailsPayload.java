9d13d0bd479c8dcaaf3fffb18799a50e09bf7a23
reyreavman <radmirkhurum@gmail.com>
2024-05-29 21:01:56 +0700

    renaming

---
+++ user-app/src/main/java/ru/rrk/user/receptionist/controller/payload/NewCheckupDetailsPayload.java
@@ -0,0 +1,14 @@
+package ru.rrk.user.receptionist.controller.payload;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record NewCheckupDetailsPayload(
+        LocalDate date,
+        LocalTime time,
+        Integer petId,
+        Integer vetId,
+        Integer checkupTypeId,
+        Integer checkupStateId,
+        Integer checkupResultId) {
+}
