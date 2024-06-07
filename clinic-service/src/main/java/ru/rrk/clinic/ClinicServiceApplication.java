7221408713bc234f1bd66e02032a965757218218
reyreavman <radmirkhurum@gmail.com>
2024-06-08 04:29:21 +0700

    entrypoints annotated

--- clinic-service/src/main/java/ru/rrk/clinic/ClinicServiceApplication.java
+++ clinic-service/src/main/java/ru/rrk/clinic/ClinicServiceApplication.java
@@ -2,8 +2,10 @@ package ru.rrk.clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClinicServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClinicServiceApplication.class, args);
